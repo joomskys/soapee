7800f40b6623299bf712afef289b85c518a8cabf
joomskys <duongmanhchinh@gmail.com>
2020-10-09 14:04:50 +0700

    commit -

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,7 @@
         'class'          => 'mb-25 mt-n25 m-lr-n20',
         'img_class'      => 'bdr-radius-t-15'
     ]); 
-    soapee_post_title(['class' => 'text-20']);
+    soapee_post_title(['class' => 'text-24']);
     soapee_post_meta(['class' => 'mb-25 pb-20']);
     soapee_post_excerpt(['class' => 'mb-25']);
     soapee_post_link_pages(['class' => 'mb-25']);
