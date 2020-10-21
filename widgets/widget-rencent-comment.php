084c0d8cda2e54ac85e9146ff84e3213d3f57c63
joomskys <duongmanhchinh@gmail.com>
2020-10-21 14:11:40 +0700

    commit -

--- widgets/widget-rencent-comment.php
+++ widgets/widget-rencent-comment.php
@@ -1,10 +1,9 @@
 <?php
-
 add_filter('show_recent_comments_widget_style', 'soapee_show_recent_comments_widget_style');
 function soapee_show_recent_comments_widget_style(){
 	return false;
 }
 
-class Soapee_WP_Widget_Recent_Comments extends WP_Widget_Recent_Comments(){
-	
+class Soapee_WP_Widget_Recent_Comments extends WP_Widget_Recent_Comments{
+
 }
